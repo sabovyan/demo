4ca74066bea69b172fbf89fab11c38daa808f761
MHER <avoyanm@gmail.com>
2022-01-27 22:31:18 +0400

    Display input changes

--- index.js
+++ index.js
@@ -1,2 +1,6 @@
-console.log("hi");
-console.log("hi");
+const myInput = document.getElementById('my-input');
+const display = document.getElementById('display');
+
+myInput.addEventListener('input', (e) => {
+  display.textContent = e.target.value;
+})
